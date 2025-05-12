077cfc8ae40ebbf15aca9bd51f51bcbd127e4eea
cathtayer <149668326+cathtayer@users.noreply.github.com>
2025-05-12 19:04:12 +0800

    update

--- assets/category-list.js
+++ assets/category-list.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
 function loadCategoryList() {
     $.ajax({
-        url: "../controllers/get_categories.php",  
+        url: "../controllers/get_categories.php",
         type: "GET",
         dataType: "json",
         success: function (data) {
