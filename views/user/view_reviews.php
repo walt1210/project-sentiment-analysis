e84fea4974a54dbe39902fb4892052f38a363c0e
cathtayer <149668326+cathtayer@users.noreply.github.com>
2025-05-16 17:35:16 +0800

    Update view_reviews.php

--- views/user/view_reviews.php
+++ views/user/view_reviews.php
@@ -8,7 +8,7 @@
   <title>Sentimo — View Reviews</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/2.2.2/css/dataTables.bootstrap5.css">
-  <link rel="stylesheet" href="/project-sentiment-analysis/assets/styles.css?v=2">
+  <link rel="stylesheet" href="../../assets/styles.css?v=2">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
